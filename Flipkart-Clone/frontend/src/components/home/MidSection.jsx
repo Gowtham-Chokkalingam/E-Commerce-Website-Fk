677526b48747bad40ca448330237c4297b6a3a77
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2022-12-12 21:29:34 +0530

    Products listing responsive done

--- Flipkart-Clone/frontend/src/components/home/MidSection.jsx
+++ Flipkart-Clone/frontend/src/components/home/MidSection.jsx
@@ -1,15 +1,37 @@
-import { Box } from "@mui/material";
+import { Box, Grid, styled } from "@mui/material";
 import React from "react";
 
 import { imageURL } from "../../constants/data";
 
+const WrapperGrid = styled(Grid)`
+  margin-top: 10px;
+  justofy-content: space-between;
+`;
+
+const Image = styled("img")(({ theme }) => ({
+  marginTop: 10,
+  width: "100%",
+  display: "flex",
+  justifyContent: "space-between",
+  [theme.breakpoints.down("md")]: {
+    ObjectFit: "cover",
+    height: "120px",
+  },
+}));
+
 const MidSection = () => {
+  const url = "https://rukminim1.flixcart.com/flap/3006/433/image/4789bc3aefd54494.jpg?q=50";
   return (
-    <Box style={{display:"flex"}}>
-      {imageURL.map((image, i) => (
-        <img style={{width:"100%"}}  src={image} alt={`Banner img ${i}`} key={i}></img>
-      ))}
-    </Box>
+    <>
+      <WrapperGrid container >
+        {imageURL.map((image, i) => (
+          <Grid item lg={4} md={4} sm={12} xs={12} key={i}>
+            <img style={{ width: "100%" }} src={image} alt={`Banner img ${i}`} ></img>
+          </Grid>
+        ))}
+      </WrapperGrid>
+      <Image src={url} alt="covid"></Image>
+    </>
   );
 };
 
--- Flipkart-Clone/frontend/src/components/home/MidSlide.jsx
+++ Flipkart-Clone/frontend/src/components/home/MidSlide.jsx
@@ -6,18 +6,24 @@ const Component = styled(Box)`
   display: flex;
 `;
 
-const LeftComponent = styled(Box)`
-  width: 82%;
-`;
+const LeftComponent = styled(Box)(({ theme }) => ({
+  width: " 82%",
+  [theme.breakpoints.down("md")]: {
+    width: " 100%",
+  },
+}));
 
-const RightComponent = styled(Box)`
-  background: #ffffff;
-  padding: 5px;
-  margin-top: 10px;
-  margin-left: 10px;
-  width: 16%;
-  text-align:center;
-`;
+const RightComponent = styled(Box)(({ theme }) => ({
+  background: "#ffffff",
+  padding: "5px",
+  marginTop: "10px",
+  marginLeft: "10px",
+  width: "16%",
+  textAlign: "center",
+  [theme.breakpoints.down("md")]: {
+    display: "none",
+  },
+}));
 
 const MidSlide = ({ products, title, timer }) => {
   const adURL = "https://rukminim1.flixcart.com/flap/464/708/image/633789f7def60050.jpg?q=70";
